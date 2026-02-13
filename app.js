e9cf00ca7e63c3bc0c5df11c60f3e8e15a554cba
sushmita patil <patilsushmitha2005@gmail.com>
2026-02-13 15:07:44 +0530

    Initial comt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Live Project - Version 2.0 🚀');
+  res.send('DevOps Live Project - Version 2.0 ');
 });
 
 app.listen(3000, () => {
